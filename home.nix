1dd38c52d39be92afa0b9afef210f2dae6976cda
Fediry <90651514+Fediry@users.noreply.github.com>
2026-01-12 10:09:24 -0800

    edited bash nrs alias in home.nix

--- home.nix
+++ home.nix
@@ -39,7 +39,7 @@
       ga = "git add ";
       gc = "git commit ";
       l = "eza --oneline --long --header --git --icons=auto --all --group-directories-first";
-      nrs = "nixos-rebuild --flake ~/nixos-dotfiles#nixos-btw switch";
+      nrs = "nixos-rebuild switch --impure --flake ~/nixos-dotfiles#nixos-btw";
     };
   };
 
